69a69a7b51fdf7d2a762ce74bb0b6011ac656a85
Arthur Cruz <arthurcruzdbp@gmail.com>
2025-12-20 12:35:34 -0300

    sql correcao

--- obj/Debug/net8.0/FitTrack.AssemblyInfo.cs
+++ obj/Debug/net8.0/FitTrack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitTrack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebc7c2781e29cec32e2f27ac44ada22cedbfa944")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+402fcb4faa2804ee21b56e87d5aaf21551a27e64")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitTrack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitTrack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
